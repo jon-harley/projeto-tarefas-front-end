8363628ddbd01a2baaf1de059f4b87271aa343de
Jonathan <jon_oliveira@outlook.com>
2019-03-18 01:10:28 -0300

    configurando Redux

---
+++ project-board-react/src/reducers/errorsReducer.js
@@ -0,0 +1,13 @@
+import {GET_ERRORS} from "../actions/types";
+
+const initialState = {error: "test erro"};
+
+export default function(state=initialState, action){
+    switch(action.type){
+        case GET_ERRORS:
+            return action.payload;
+        
+        default:
+            return state;
+    }
+}
